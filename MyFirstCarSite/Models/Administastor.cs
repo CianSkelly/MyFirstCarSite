408df95157c5fa7b15b772da81f308e64f776269
Cian Skelly <cianskelly@hotmail.com>
2018-10-15 20:33:51 +0100

    201810152035 - copy and paste the following bootstrap css file to try to fix my styling and it worked, I have a style to the site: <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">

---
+++ MyFirstCarSite/Models/Administastor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCarSite.Models
+{
+    public class Administastor
+    {
+        public int AdminID { get; set; }
+        public string AdminName { get; set; }
+    }
+}
